04e655b622b870a3fc28980930ca07bafef5d3d1
YvanBrayn <Yvan.Brayn@outlook.com>
2021-08-05 13:03:01 -0300

    aula_1
    Olá Mundo!!!

---
+++ cursoProgramacao/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main { //Classe basica "Main"
+
+	public static void main(String[] args) { //função principal, para a execução do programa
+		
+		System.out.println("Olá Mundo!!!"); //Sistem.out.print (imprimir na tela) ln para ser na linha de baixo
+
+	}
+
+}
